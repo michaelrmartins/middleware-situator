12dc5d9262e4f5106f50b28606d875e98bdd3fd7
m1k3 <michaelmartins.js@hotmail.com>
2026-03-13 14:29:11 -0300

    refactor: update comments for person route filters for clarity

--- src/api/v1/routes/person-route.js
+++ src/api/v1/routes/person-route.js
@@ -12,19 +12,11 @@ router.get('/persons', personController.getAllPersons);
 router.get('/persons/active', personController.getActivePersons);
 router.get('/persons/inactive', personController.getInactivePersons);
 
-// Route to get person data by ID
+// Route to get person data by filters
 router.get('/id/:id', personController.getPersonById);
-
-// Route to get person data by document
 router.get('/document/:document', personController.getPersonByDocument);
-
-// Route to get persons data by name
 router.get('/name/:name', personController.getPersonsByName);
-
-// Route to get person data by card number
 router.get('/card/:cardNumber', personController.getPersonByCardNumber);
-
-// Route to get person data by card number hex
 router.get('/card/hex/:cardNumberHex', personController.getPersonByCardNumberHex);
 
 module.exports = router;
