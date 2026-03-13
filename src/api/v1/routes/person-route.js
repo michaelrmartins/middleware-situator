da85b376b50e3351fe353fc84537cc4e94cb3f42
m1k3 <michaelmartins.js@hotmail.com>
2026-03-13 12:14:24 -0300

    refactor: comment out unused person data retrieval routes for clarity

--- src/api/v1/routes/person-route.js
+++ src/api/v1/routes/person-route.js
@@ -7,6 +7,10 @@ const personController = require('../controllers/person-controller');
 // Root route for testing
 router.get('/', (req, res) => {res.status(200).send('Person route is working!');});
 
+// Route to get person data by ID
+// router.get('/id/:id', personController.getPersonById);
+// router.get('/document/:document', personController.getPersonByDocument);
+
 // Route to get all persons data
 router.get('/persons', personController.getAllPersons);
 router.get('/persons/active', personController.getActivePersons);
