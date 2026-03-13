b19b6b4c34673769f8b91ba5a400378ac756e201
m1k3 <michaelmartins.js@hotmail.com>
2026-03-13 12:14:15 -0300

    refactor: streamline person data retrieval by replacing db queries with executeQuery utility

--- src/api/v1/models/model.js
+++ src/api/v1/models/model.js
@@ -1,41 +1,57 @@
-const db = require('../../../config/database');
-const { selectAllPersonsQuery, selectAllActivePersonsQuery, selectAllInactivePersonsQuery } = require('./query/person-query');
+const {executeQuery} = require('../../../utils/execute-query');
+
+const { selectAllPersonsQuery,
+        selectAllPersonsByNameQuery,
+        selectAllActivePersonsQuery,
+        selectAllInactivePersonsQuery,
+        selectPersonDatabyIdQuery,
+        selectPersonDatabyDocumentQuery,
+        selectPersonDatabyCardNumberQuery,
+        selectPersonDatabyCardNumberHexQuery } = require('./query/person-query');
+
+async function getPersonByIdModel(id) {
+    const rows = await executeQuery(selectPersonDatabyIdQuery, [id]);
+    return rows[0];
+}
 
 // Get all persons model
 async function getAllPersonsModel() {
-    try {
-        const result = await db.query(selectAllPersonsQuery);
-        return result.rows;
-    } catch (error) {
-        console.error('Error fetching persons data:', error);
-        throw error;
-    }
+    return executeQuery(selectAllPersonsQuery);
 }
 
 // Get active persons model
 async function getActivePersonsModel() {
-    try {
-        const result = await db.query(selectAllActivePersonsQuery);
-        return result.rows;
-    } catch (error) {
-        console.error('Error fetching active persons data:', error);
-        throw error;
-    }
+return executeQuery(selectAllActivePersonsQuery);
 }
 
 // Get inactive persons model
 async function getInactivePersonsModel() {
-    try {
-        const result = await db.query(selectAllInactivePersonsQuery);
-        return result.rows;
-    } catch (error) {
-        console.error('Error fetching inactive persons data:', error);
-        throw error;
-    }
+ return executeQuery(selectAllInactivePersonsQuery);
+}
+
+async function getPersonsByNameModel(name) {
+    return executeQuery(selectAllPersonsByNameQuery, [name]);
+}
+
+async function getPersonByDocumentModel(document) {
+    return executeQuery(selectPersonDatabyDocumentQuery, [document]);
+}
+
+async function getPersonByCardNumberModel(cardNumber) {
+    return executeQuery(selectPersonDatabyCardNumberQuery, [cardNumber]);
+}
+
+async function getPersonByCardNumberHexModel(cardNumberHex) {
+    return executeQuery(selectPersonDatabyCardNumberHexQuery, [cardNumberHex]);
 }
 
 module.exports = {
+    getPersonByIdModel,
     getAllPersonsModel,
     getActivePersonsModel,
-    getInactivePersonsModel
+    getInactivePersonsModel,
+    getPersonsByNameModel,
+    getPersonByDocumentModel,
+    getPersonByCardNumberModel,
+    getPersonByCardNumberHexModel
 };
